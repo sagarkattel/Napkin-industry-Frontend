64563262790540d7c76a298806f51ae1be360753
35anurag <anuragregmi35@gmail.com>
2023-11-20 15:26:40 +0545

    fix homepage

--- src/app/home/banner.tsx
+++ src/app/home/banner.tsx
@@ -6,7 +6,7 @@ import { Carousel, CarouselProps } from "@material-tailwind/react";
 
 
 
-const Banner:React.FC = () => {
+const Banner = () => {
   const [activeIndex, setActiveIndex] = useState<number>(0);
   const length:number = 3; // Update this with the actual number of images
 
